77a7bdc1dcf33520c4831726747879d91c71dd2e
rsperberg <rsperberg@gmail.com>
2014-08-01 02:32:05 +0000

    Completed 3 exercises using if-else statements

--- 05-booleans/00-basics/spec.js
+++ 05-booleans/00-basics/spec.js
@@ -1,54 +1,55 @@
+/*eslint-env node, mocha */
 var expect = require('chai').expect;
 var challenge = require('./challenge');
 
-describe("inBetween", function() {
-  it("should be defined", function() {
+describe('inBetween', function() {
+  it('should be defined', function() {
     expect(challenge.inBetween).to.exist;
   });
 
-  it("should return true if the middle number is in between the lower and upper number", function() {
-    expect(challenge.inBetween(1,3,5)).to.be.true
+  it('should return true if the middle number is in between the lower and upper number', function() {
+    expect(challenge.inBetween(1,3,5)).to.be.true;
   });
 
-  it("should return false if middle is not in between lower and upper", function() {
+  it('should return false if middle is not in between lower and upper', function() {
     expect(challenge.inBetween(3,1,5)).to.be.false;
   });
 });
 
-describe("equalStrings", function() {
-  it("should be defined", function() {
+describe('equalStrings', function() {
+  it('should be defined', function() {
     expect(challenge.equalStrings).to.exist;
   });
 
-  it("should return true if strings are equal", function() {
-    expect(challenge.equalStrings('bloc', 'bloc')).to.be.true
+  it('should return true if strings are equal', function() {
+    expect(challenge.equalStrings('bloc', 'bloc')).to.be.true;
   });
 
-  it("should return false if strings aren't equal", function () {
-    expect(challenge.equalStrings('dan', 'mark')).to.be.false
+  it('should return false if strings aren\'t equal', function () {
+    expect(challenge.equalStrings('dan', 'mark')).to.be.false;
   });
 });
 
-describe("notEqual", function() {
-  it("should be defined", function() {
+describe('notEqual', function() {
+  it('should be defined', function() {
     expect(challenge.notEqual).to.exist;
   });
 
-  it("should return true if the two arguments are not the same value or type", function() {
+  it('should return true if the two arguments are not the same value or type', function() {
     expect(challenge.notEqual(1,'1')).to.be.true;
   });
 
-  it("should return false if the two arguments are the same type and value", function () {
+  it('should return false if the two arguments are the same type and value', function () {
     expect(challenge.notEqual('bloc','bloc')).to.be.false;
   });
 });
 
-describe("outsideRanges", function() {
-  it("should be defined", function() {
+describe('outsideRanges', function() {
+  it('should be defined', function() {
     expect(challenge.outsideRanges).to.exist;
   });
 
-  it("should return true for random numbers outside of ranges", function() {
+  it('should return true for random numbers outside of ranges', function() {
     expect(challenge.outsideRanges(0)).to.be.true;
     expect(challenge.outsideRanges(7)).to.be.true;
     expect(challenge.outsideRanges(-100)).to.be.true;
@@ -56,7 +57,7 @@ describe("outsideRanges", function() {
     expect(challenge.outsideRanges(35)).to.be.true;
   });
 
-  it("should return true for boundary numbers outside all ranges", function() {
+  it('should return true for boundary numbers outside all ranges', function() {
     expect(challenge.outsideRanges(1)).to.be.true;
     expect(challenge.outsideRanges(6)).to.be.true;
     expect(challenge.outsideRanges(9)).to.be.true;
@@ -64,33 +65,33 @@ describe("outsideRanges", function() {
     expect(challenge.outsideRanges(42)).to.be.true;
   });
 
-  it("should return false for boundary numbers", function() {
+  it('should return false for boundary numbers', function() {
     expect(challenge.outsideRanges(10)).to.be.false;
     expect(challenge.outsideRanges(20)).to.be.false;
     expect(challenge.outsideRanges(75)).to.be.false;
   });
 });
 
-describe("nameAndPrice", function() {
-  it("should be defined", function() {
+describe('nameAndPrice', function() {
+  it('should be defined', function() {
     expect(challenge.nameAndPrice).to.exist;
   });
 
-  it("should return true for our papers and prices greater than or equal to 1.", function() {
-    expect(challenge.nameAndPrice("NYTimes", 1)).to.be.true;
-    expect(challenge.nameAndPrice("LATimes", 1)).to.be.true;
+  it('should return true for our papers and prices greater than or equal to 1.', function() {
+    expect(challenge.nameAndPrice('NYTimes', 1)).to.be.true;
+    expect(challenge.nameAndPrice('LATimes', 1)).to.be.true;
   });
 
-  it("should return false for LATimes and price less than 1.", function() {
-    expect(challenge.nameAndPrice("LATimes", .5)).to.be.false;
+  it('should return false for LATimes and price less than 1.', function() {
+    expect(challenge.nameAndPrice('LATimes', .5)).to.be.false;
   });
 
-  it("should return false for NYTimes and price less than 1.", function() {
-    expect(challenge.nameAndPrice("NYTimes", .5)).to.be.false;
+  it('should return false for NYTimes and price less than 1.', function() {
+    expect(challenge.nameAndPrice('NYTimes', .5)).to.be.false;
   });
 
-  it("should return false for Chicago Tribune and any price.", function() {
-    expect(challenge.nameAndPrice("Chicago Tribune", 1)).to.be.false;
-    expect(challenge.nameAndPrice("Chicago Tribune", 0.5)).to.be.false;
+  it('should return false for Chicago Tribune and any price.', function() {
+    expect(challenge.nameAndPrice('Chicago Tribune', 1)).to.be.false;
+    expect(challenge.nameAndPrice('Chicago Tribune', 0.5)).to.be.false;
   });
 });
--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,66 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(fav, guess) {
+   if (guess > fav) {
+      return 'Too high';
+   } else if (guess < fav) {
+      return 'Too low';
+   } else {
+      return 'You got it!';
+   }
+};
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(a, b, c, d) {
+   if ( !(typeof a  === 'number') ) {
+      return 'incorrect';
+   } else if ( !(typeof b  === 'number') ) {
+      return 'incorrect';
+    } else if ( !(typeof c  === 'number') ) {
+      return 'incorrect';
+   } else if ( !(typeof d  === 'number') ) {
+      return 'incorrect';
+   } else if ( !( (a === 3) || (a === 5) || (a === 7)) ) {
+      return 'incorrect';
+    } else if ( !(b === 2)) {
+      return 'incorrect';
+   } else if ( !((c >= 5) && (c <= 100)) ) {
+      return 'incorrect';
+   } else if (!(d < 9) && !(d > 20)) {
+      return 'incorrect';
+   } else {
+      return 'correct';
+ }
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+if (item === 'MacBook Air') {
+   if (money >= 899) {
+      return true;
+   } else {
+      return false;
+   }
+} else if (item === 'MacBook Pro') {
+   if (money >= 1099) {
+      return true;
+   } else {
+      return false;
+   }
+} else if (item === 'Mac Pro') {
+   if (money >= 2999) {
+      return true;
+   } else {
+      return false;
+   }
+} else if (item === 'Apple Sticker') {
+   if (money >= 1) {
+      return true;
+   } else {
+      return false;
+   }
+} else {
+   return false;
+}
+};
+
+
+/*
+canIGet('MacBook Air', 100) // returns false
+*/
